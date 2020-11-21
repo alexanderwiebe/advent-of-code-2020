5cfc5d5974535cc7e4e0f3d527e4a69269880db3
aj <aj@softwarebyaj.com>
2020-11-21 19:50:55 +0000

    got debugging working

---
+++ Program.fs
@@ -0,0 +1,11 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+
+let asdf = "wut up"
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+    let fdsa = asdf + " asdf"
+    0 // return an integer exit code
