972babaa9d2da264bf09b1fb60ffeac92496897a
aj <aj@softwarebyaj.com>
2020-11-30 14:43:24 +0000

    i think i got the main portion working

--- services.test/Day1.test.fs
+++ services.test/Day1.test.fs
@@ -6,6 +6,6 @@ open services
 
 [<Fact>]
 let ``should convert strings to ints`` () =
-    let expected = [2; 4; 6; 8; 10]
-    let actual = Day1.findDoubles [|"2"; "4"; "6"; "8"; "10"|]
+    let expected = 100
+    let actual = Day1.findDoubles [|"2"; "4"; "6"; "8"; "10"; "10"|]
     Assert.Equal(expected, actual)
--- services/Day1.fs
+++ services/Day1.fs
@@ -3,5 +3,4 @@
 open System
 
 module Day1 =
-    let findDoubles list = list |> Seq.map System.Int32.Parse
-        
+    let findDoubles list = list |> Seq.map System.Int32.Parse |> Seq.pairwise |> Seq.find (fun elem -> fst elem + snd elem = 20) |> fun elem -> fst elem * snd elem
