3dd9f8bd059320dea29555b3619b23e9271efc0a
aj <aj@softwarebyaj.com>
2020-12-02 21:27:44 -0600

    forgot that regex will match if the string is longer than the target

--- Program.fs
+++ Program.fs
@@ -4,10 +4,10 @@ open services
 
 [<EntryPoint>]
 let main argv =
-  printfn "Welcome to advent of code day 1"
-  let parseFile = File.ReadAllLines("day01.data")
+  printfn "Welcome to advent of code day 2"
+  let parseFile = File.ReadAllLines("day02.data")
   
-  let total = Day01.findDoubles parseFile
+  let total = Day02.runner parseFile
 
   printfn "total: %i" total
   Console.ReadKey() |> ignore
--- services/Day02.fs
+++ services/Day02.fs
@@ -1,6 +1,23 @@
 ﻿namespace services
 
 open System
+open System.Text.RegularExpressions
 
 module Day02 =
     let parsePassword (passwordConfig:string) : string [] = passwordConfig.Split(" ")
+    
+    
+    let createRegex (passwordParsed: string[]) :string =
+        match passwordParsed with
+        | [| quantity; seek; _; |] ->
+            let regexes = quantity.Split("-")
+            $"{seek.Substring(0,1)}{{{regexes.[0]},{regexes.[1]}}}"
+            
+    let checkRegex text regex =
+        if Regex.IsMatch(text, regex) then 1
+        else 0
+        
+    let runner passwordList =
+        passwordList |> Seq.map parsePassword
+        |> Seq.map (fun passParts -> passParts |> createRegex |> checkRegex (Regex.Replace(passParts.[2], $"[^{passParts.[1].Substring(0,1)}]", "")))
+        |> Seq.sum
