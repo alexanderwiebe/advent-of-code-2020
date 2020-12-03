8d99fbf34fa6ed90cae3fe535568ad1e179de2c3
aj <aj@softwarebyaj.com>
2020-12-02 21:47:28 -0600

    moved to stripping out the old  day 2 done

--- services/Day02.fs
+++ services/Day02.fs
@@ -13,11 +13,22 @@ module Day02 =
             let regexes = quantity.Split("-")
             $"{seek.Substring(0,1)}{{{regexes.[0]},{regexes.[1]}}}"
             
+    let createMinMax (passwordParsed: string[]) :(int * int) =
+        match passwordParsed with
+        | [| quantity; seek; _; |] ->
+            let regexes = quantity.Split("-")
+            (Int32.Parse regexes.[0], Int32.Parse regexes.[1])
+            
     let checkRegex text regex =
         if Regex.IsMatch(text, regex) then 1
         else 0
-        
+    
+    let checkRange (text:string) (minMax:int * int) =
+        if text.Length >= fst minMax && text.Length <= snd minMax then 1
+        else 0
+      
     let runner passwordList =
         passwordList |> Seq.map parsePassword
-        |> Seq.map (fun passParts -> passParts |> createRegex |> checkRegex (Regex.Replace(passParts.[2], $"[^{passParts.[1].Substring(0,1)}]", "")))
+        |> Seq.map (fun passParts -> passParts |> createMinMax |> checkRange (Regex.Replace(passParts.[2], $"[^{passParts.[1].Substring(0,1)}]", ""))) 
+// |> checkRegex (Regex.Replace(passParts.[2], $"[^{passParts.[1].Substring(0,1)}]", "")))
         |> Seq.sum
